1937f7f23237122ad8de53f97da15e0fe6208c4b
Marcos <marcoseduc2019@gmail.com>
2024-09-02 10:37:05 -0300

    consertando menu do mobile

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,42 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Sidebar.css';
 
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(false);
-  const [buttonRect, setButtonRect] = useState({});
 
   const toggleSidebar = () => {
     setShowSidebar(!showSidebar);
   };
 
-  useEffect(() => {
-    const button = document.querySelector('.toggle-btn');
-    if (button) {
-      setButtonRect(button.getBoundingClientRect());
-    }
-  }, [showSidebar]);
-
   return (
     <>
+      <div className={`floating-menu ${showSidebar ? 'show' : 'hide'}`}>
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/midia">Mídia</a></li>
+            <li><a href="/Briefings">Briefings</a></li>
+            <li><a href="/Vendas">Vendas</a></li>
+            <li><a href="/Aluguel">Aluguel</a></li>
+          </ul>
+        </nav>
+      </div>
       <button className="toggle-btn" onClick={toggleSidebar}>
-        ☰
+        ☰ Menu
       </button>
       {showSidebar && <div className="blur-background"></div>}
-      <div
-        className={`floating-menu ${showSidebar ? 'show' : 'hide'}`}
-        style={{
-          top: buttonRect.top + buttonRect.height + window.scrollY + 10 + 'px', 
-          left: buttonRect.left + 'px',
-        }}
-      >
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/midia">Mídia</a></li>
-          <li><a href="/Briefings">Briefings</a></li>
-          <li><a href="/Vendas">Vendas</a></li>
-          <li><a href="/Aluguel">Aluguel</a></li>
-        </ul>
-      </div>
       <nav className="horizontal-menu">
         <ul>
           <li><a href="/">Home</a></li>
