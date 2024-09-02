b3d0f97ef47a0a795637dc01ca0a6fb4f2f693a0
Marcos <marcoseduc2019@gmail.com>
2024-09-02 10:29:47 -0300

    teste

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,20 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Sidebar.css';
 
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(false);
+  const [buttonRect, setButtonRect] = useState({});
 
   const toggleSidebar = () => {
     setShowSidebar(!showSidebar);
   };
 
+  useEffect(() => {
+    const button = document.querySelector('.toggle-btn');
+    if (button) {
+      setButtonRect(button.getBoundingClientRect());
+    }
+  }, [showSidebar]);
+
   return (
     <>
       <button className="toggle-btn" onClick={toggleSidebar}>
-        ☰ Menu
+        ☰
       </button>
-      {showSidebar && <div className="blur-background"></div>} {/* Fundo embaçado somente quando o menu estiver aberto */}
-      <div className={`floating-menu ${showSidebar ? 'show' : 'hide'}`}>
+      {showSidebar && <div className="blur-background"></div>}
+      <div
+        className={`floating-menu ${showSidebar ? 'show' : 'hide'}`}
+        style={{
+          top: buttonRect.top + buttonRect.height + window.scrollY + 10 + 'px', 
+          left: buttonRect.left + 'px',
+        }}
+      >
         <ul>
           <li><a href="/">Home</a></li>
           <li><a href="/midia">Mídia</a></li>
@@ -23,6 +37,15 @@ const Sidebar = () => {
           <li><a href="/Aluguel">Aluguel</a></li>
         </ul>
       </div>
+      <nav className="horizontal-menu">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/midia">Mídia</a></li>
+          <li><a href="/Briefings">Briefings</a></li>
+          <li><a href="/Vendas">Vendas</a></li>
+          <li><a href="/Aluguel">Aluguel</a></li>
+        </ul>
+      </nav>
     </>
   );
 };
